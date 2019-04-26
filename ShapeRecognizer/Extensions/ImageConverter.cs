bcae264855a9ee129e5791d543c2a3fe0924d0e7
Matthew Leibowitz <mattleibow@live.com>
2019-04-26 23:13:59 +0400

    reorganized some files

---
+++ ShapeRecognizer/Extensions/ImageConverter.cs
@@ -0,0 +1,21 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ShapeRecognizer
+{
+	public class ImageConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is SKImage image)
+				return (SKImageImageSource)image;
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+			throw new NotSupportedException();
+	}
+}
